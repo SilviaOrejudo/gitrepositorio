63bf24769871f2252f5ea2094f6dc57c3d24171f
SilviaOrejudo <sorejudoc02@iesalbarregas.es>
2021-03-17 20:35:06 +0100

    Líneas en método main

--- Silvia_OrejudoCarmonaDAM/src/Tarea6Tema4/prueba1.java
+++ Silvia_OrejudoCarmonaDAM/src/Tarea6Tema4/prueba1.java
@@ -5,6 +5,8 @@ public class prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
+		System.out.println("Líneas en el método main");
+		System.out.println("Otra línea");
 
 	}
 
